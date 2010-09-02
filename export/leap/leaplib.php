e672f8e051e900ae14eb723e47afe7b504927a28
Dan Marsden <dan@danmarsden.com>
2010-09-02 12:20:05 +1200

    Infolio - tidy up LEAP export and push to browser when created.

--- export/leap/leaplib.php
+++ export/leap/leaplib.php
@@ -10,7 +10,7 @@
  * @license    http://creativecommons.org/licenses/by-nc-sa/2.0/uk/
 */
 function leap_header($user, $export_time) {
-    return "<? xml version=\"1.0\" encoding=\"utf-8\"?>
+    return "<?xml version=\"1.0\" encoding=\"utf-8\"?>
 <feed xmlns=\"http://www.w3.org/2005/Atom\"
     xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"
     xmlns:leap=\"http://wiki.cetis.ac.uk/2009-03/LEAP2A_predicates#\"
@@ -72,15 +72,35 @@ function leap_entry($entry) {
     $output .= ($entry->contenttype == 'xhtml') ? "<div xmlns=\"http://www.w3.org/1999/xhtml\">" : '';
     $output .= (!empty($entry->content)) ? $entry->content : '';
     $output .= ($entry->contenttype == 'xhtml') ? "</div>" : '';
-    $output .= "</content>
+    $output .= "</content>";
+    if (!empty($entry->resource)) {
+        $output .= $entry->resource;
+    } else {
+    $output .= "
         <rdf:type rdf:resource=\"leaptype:$entry->leaptype\"/>";
+    }
     return $output;
 }
 function leap_blocks($page, $studentUser) {
     global $studentTheme;
     //TODO: add Infolio specific content here for blocks to allow correct restore.
-    return '<div>'.$page->HtmlTitle().'<div id="blocks">'.
-               $page->HtmlBlocks($studentTheme, $studentUser) . '</div></div>';
+    
+    $output = '<div>'.$page->HtmlTitle().'<div id="blocks">';
+    //$page->HtmlBlocks($studentTheme, $studentUser) 
+    $sql = "SELECT * FROM block WHERE page_id='".$page->getId()."' AND user_id='". $studentUser->getId()."'";
+    $db = Database::getInstance();
+    $result = $db->query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $output .= "<p>".$row['words0']."</p>";
+        if (!empty($row['picture0'])) {
+            $output .= "<a rel=\"leap2:has_part\" href=\"portfolio:artefact".$row['picture0']."\"><img rel=\"leap2:has_part\" href=\"portfolio:artefact".$row['picture0']."\" /></a>";
+        }
+        if (!empty($row['picture1'])) {
+            $output .= "<a rel=\"leap2:has_part\" href=\"portfolio:artefact".$row['picture1']."\"><img rel=\"leap2:has_part\" href=\"portfolio:artefact".$row['picture1']."\" /></a>";
+        }
+    }
+    $output .='</div></div>';
+    return $output;
 }
 function leap_categories() {
     
@@ -95,5 +115,22 @@ function leap_links() {
 function leap_view() {
     
 }
+function leap_resource($resource) {
+    $output = "
+    <rdf:type rdf:resource=\"leap2:resource\" />
+<category scheme=\"categories:resource_type#\" term=\"Offline\" />
+<mahara:artefactplugin mahara:type=\"image\" mahara:plugin=\"file\"/>
+<link rel=\"enclosure\" type=\"$resource->contenttype\" href=\"files/$resource->url\" />";
+    
+    //TODO - attach links to resources -db query against blocks to find all items with image 1 or 2 set to this id.
+    $sql = "SELECT * FROM block WHERE picture0='". $resource->id."' OR picture1='". $resource->id."'";
+    $db = Database::getInstance();
+    $result = $db->query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $output .= "<link rel=\"leap2:is_part_of\" href=\"portfolio:view".$row['id']."\"/>";
+    }
+    return $output;
+}
+
 
 
