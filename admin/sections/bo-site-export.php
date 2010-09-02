8cf57f20484dfc9403ed6f82c54029ad8a6121c7
Dan Marsden <dan@danmarsden.com>
2010-09-02 22:12:34 +1200

    add missing export file

---
+++ admin/sections/bo-site-export.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * leaplib.php - Creates a LEAP export of a user's infolio
+ * See http://wiki.cetis.ac.uk/LEAP2A_specification
+ *
+ * LICENSE: This is an Open Source Project
+ *
+ * @author     Dan Marsden, Catalyst IT Ltd
+ * @copyright  2010 Catalyst IT Ltd
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.0/uk/
+*/
+
+        $html = '<form method="post" action="/export.php"><ul style="list-style:none;">';
+        $html .= '<input type="hidden" name="tab_count" value="0" />';
+        $html .= '</ul>';
+        $html .= "<h3>Choose an export format</h3><div class=\"element\"><div><input type=\"radio\" class=\"radio\" id=\"export_formate091\" name=\"format\" tabindex=\"1\" value=\"html\" checked=\"checked\">"
+                ."<label for=\"export_formate091\">Standalone HTML Website</label><div class=\"radio-description\">Creates a self-contained website for each user with their portfolio data. You cannot import this again, but it's readable in a standard web browser.</div></div><div>"
+                ."<input type=\"radio\" class=\"radio\" id=\"export_format442a\" name=\"format\" tabindex=\"1\" value=\"leap\"> <label for=\"export_format442a\">LEAP2A</label><div class=\"radio-description\">Gives you an export for each user in the LEAP2A standard format. You can use this to import your data into other LEAP2A compliant systems, although the export is hard for humans to read.</div></div></div>";
+
+        $html .= '<input type="hidden" name="siteexport" value="true" />';
+        $html .= '<input type="submit" value="Generate new site export" />';
+
+        $html .= '</form>';
+        echo $html;
